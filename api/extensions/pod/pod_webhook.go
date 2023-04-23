3006f3350707b1b4122d997e3bd8731ab8cfa094
yumaojun <18108053819@163.com>
2023-04-23 08:24:18 +0800

    补充pod env注入

--- api/extensions/pod/pod_webhook.go
+++ api/extensions/pod/pod_webhook.go
@@ -4,16 +4,20 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/infraboard/mpaas/apps/deploy"
+	mpaas "github.com/infraboard/mpaas/client/rpc"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
-	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 type PodWebHook struct {
+	mpaas *mpaas.ClientSet
 }
 
 func (r *PodWebHook) SetupWebhookWithManager(mgr ctrl.Manager) error {
+	r.mpaas = mpaas.C()
 	return ctrl.NewWebhookManagedBy(mgr).
 		For(&corev1.Pod{}).
 		WithDefaulter(&PodWebHook{}).
@@ -22,7 +26,7 @@ func (r *PodWebHook) SetupWebhookWithManager(mgr ctrl.Manager) error {
 
 // Default implements webhook.Defaulter so a webhook will be registered for the type
 func (r *PodWebHook) Default(ctx context.Context, obj runtime.Object) error {
-	log := logf.FromContext(ctx)
+	l := log.FromContext(ctx)
 	pod, ok := obj.(*corev1.Pod)
 	if !ok {
 		return fmt.Errorf("expected a Pod but got a %T", obj)
@@ -32,6 +36,42 @@ func (r *PodWebHook) Default(ctx context.Context, obj runtime.Object) error {
 		pod.Annotations = map[string]string{}
 	}
 	pod.Annotations["example-mutating-admission-webhook"] = "foo"
-	log.Info("Annotated Pod")
+	l.Info("Annotated Pod")
+
+	// 处理deploy注解: deploy.mpaas.inforboard.io/id
+	err := r.HandlePodEnvInject(ctx, pod)
+	if err != nil {
+		l.Error(err, "hanle deploy error")
+	}
+
+	return nil
+}
+
+func (r *PodWebHook) HandlePodEnvInject(ctx context.Context, obj *corev1.Pod) error {
+	// 获取日志对象
+	l := log.FromContext(ctx)
+
+	// 根据注解获取task id
+	deployId := obj.Annotations[deploy.ANNOTATION_DEPLOY_ID]
+	if deployId == "" {
+		return nil
+	}
+	l.Info(fmt.Sprintf("get mpaas deploy: %s", deployId))
+
+	// 查询Pod需要注入的Env变量
+	queryEnv := deploy.NewQueryDeploymentInjectEnvRequest(deployId)
+	set, err := r.mpaas.Deploy().QueryDeploymentInjectEnv(ctx, queryEnv)
+	if err != nil {
+		return fmt.Errorf("get deploy error, %s", err)
+	}
+
+	// 注入变量
+	for i := range set.EnvGroups {
+		group := set.EnvGroups[i]
+		if group.Enabled {
+
+		}
+	}
+
 	return nil
 }
